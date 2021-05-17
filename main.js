ca81513417cee37d2c0f8977fc1071ed02b71061
fherrera <facundoh1986@gmail.com>
2021-05-17 15:25:15 +0200

    Primera prueba de playwright

---
+++ main.js
@@ -0,0 +1,14 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+  const context = await browser.newContext();
+
+  const page = await context.newPage();
+  await page.goto('https://web.gencat.cat/ca/inici');
+
+  await page.fill('#cercadorOcultGoogle', 'agenda cultural');
+  await page.click('[aria-label="Cercar"]');
+
+  //await browser.close();
+})();
